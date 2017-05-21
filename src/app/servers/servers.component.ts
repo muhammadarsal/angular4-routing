ba23c12415dbd23410e48b941595cf5d33297e0e
Muhammad Arsal Abdullah <arsal@plerosys.com>
2017-05-21 16:27:40 +0500

    8-programmatic navigation

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { ServersService } from './servers.service';
 
 @Component({
@@ -9,10 +10,23 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   private servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+  	private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
+  }
+
+  onReload() {
+  	// navigate() doesn't know which link we're currently on
+  	// to tell it the current route we need to pass a configuration
+  	// object as a second argument with relativeTo property
+
+  	// if we don't specify relativeTo, our path is always relative to
+  	// the root path
+
+  	// now the following line of code will break the app:
+  	// this.router.navigate(['servers'], {relativeTo: this.route});
   }
 
 }
